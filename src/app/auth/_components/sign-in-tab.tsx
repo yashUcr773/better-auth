f828a69c9389cf9aeaf107b3e3aea964e582090f
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 14:17:24 +0530

    sign in page

--- src/app/auth/_components/sign-in-tab.tsx
+++ src/app/auth/_components/sign-in-tab.tsx
@@ -1,3 +1,103 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoadingSwap } from "@/components/ui/loading-swap";
+import { PasswordInput } from "@/components/ui/password-input";
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+const signInSchema = z.object({
+  email: z.email().min(1),
+  password: z.string().min(8),
+});
+
+type SignInForm = z.infer<typeof signInSchema>;
+
 export function SignInTab() {
-  return <div>Sign in</div>
+  const router = useRouter();
+
+  const form = useForm<SignInForm>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSignIn = async (data: SignInForm) => {
+    const res = await authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+        callbackURL: "/",
+      },
+      {
+        onError: (error) => {
+          console.log("🚀 ~ handleSignUp ~ error:", error);
+          toast.error(error.error.message || "Failed to Sign In!");
+        },
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Sign In Successful!");
+        },
+      }
+    );
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(handleSignIn)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          disabled={form.formState.isSubmitting}
+          className="w-full"
+        >
+          <LoadingSwap isLoading={form.formState.isSubmitting}>
+            Sign In
+          </LoadingSwap>
+        </Button>
+      </form>
+    </Form>
+  );
 }
