5079d2cadaa2847cc150a9a9bb3bf6ce5bdc7df8
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 18:34:40 +0530

    basic rate limit

--- src/drizzle/schemas/auth-schema.ts
+++ src/drizzle/schemas/auth-schema.ts
@@ -1,5 +1,13 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  bigint,
+  timestamp,
+  boolean,
+  integer,
+  index,
+} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -72,6 +80,13 @@ export const verification = pgTable(
   },
   (table) => [index("verification_identifier_idx").on(table.identifier)],
 );
+
+export const rateLimit = pgTable("rate_limit", {
+  id: text("id").primaryKey(),
+  key: text("key"),
+  count: integer("count"),
+  lastRequest: bigint("last_request", { mode: "number" }),
+});
 
 export const userRelations = relations(user, ({ many }) => ({
   sessions: many(session),
